fca4bd30ea748563f5799c328e1e8f8bf0f9a030
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-21 09:19:32 -0600

    use counter screen as default

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:counter_app/screens/home_screen.dart';
+import 'package:counter_app/screens/counter_screen.dart';
 
 main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: CounterScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
